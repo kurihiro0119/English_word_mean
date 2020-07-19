d97d654ea14f3cbd7ee5e9683d1162042014d642
kurihiro0119 <54324711+kurihiro0119@users.noreply.github.com>
2020-07-19 11:54:17 +0900

    organize

--- word_get/serializers.py
+++ word_get/serializers.py
@@ -4,13 +4,6 @@ from .english_word import get_mean_word
 import json
 
 
-class WordSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Word
-        fields = ['word', 'mean']
-
-
 class GetWordsSerializer(serializers.Serializer):
 
     word = serializers.CharField()
@@ -21,31 +14,3 @@ class GetWordsSerializer(serializers.Serializer):
             return None
         mean = get_mean_word(obj['word'])
         return mean
-
-
-class WordSerializerField(serializers.Field):
-
-    mean = None
-
-    def to_value(self, data):
-        self.mean = get_mean_word(data.word)
-        return self.mean
-
-
-class MyWordSerializer(serializers.ModelSerializer):
-    mean = serializers.SerializerMethodField()
-    # mean = WordSerializerField()
-
-    class Meta:
-        model = Word
-        fields = ['word', 'mean']
-
-    def get_mean(self, obj):
-        if obj.word is None:
-            return None
-        mean = get_mean_word(obj.word)
-        return mean
-
-    # def is_named_bar(self, word):
-    #     mean = get_mean_word(word.word)
-    #     return mean
